7b41ff68a3a6d4e3488d189d5c098b6e07186796
Maciej Janusz Krajsman <mjkrajsman@gmail.com>
2019-03-21 17:12:01 +0100

    Add everything done during lesson 2

--- AndroidLevelUp2/app/src/main/java/com/daftmobile/androidlevelup/ConstraintSetActivity.kt
+++ AndroidLevelUp2/app/src/main/java/com/daftmobile/androidlevelup/ConstraintSetActivity.kt
@@ -1,12 +1,44 @@
 package com.daftmobile.androidlevelup
 
 import android.os.Bundle
+import android.transition.TransitionManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import kotlinx.android.synthetic.main.activity_square.*
 
 class ConstraintSetActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_square)
+//        nextConstraints = ConstraintSet().apply {
+//            clone(contentView.context, R.layout.activity_square_alter)
+//        }
+
+        square.setOnClickListener{
+            relayout(contentView)
+        }
     }
+
+    private fun relayout(contentView: ConstraintLayout){
+        // create new constraints
+//        val oldConstraints = ConstraintSet().apply{
+//            clone(contentView)
+//        }
+
+        val newConstraints = ConstraintSet()
+
+        newConstraints.apply {
+            clone(contentView.context, R.layout.activity_square_alter)
+        }
+
+
+        // begin transition
+        TransitionManager.beginDelayedTransition(contentView)
+
+        // apply new constraints
+        newConstraints.applyTo(contentView)
+    }
+
 }
